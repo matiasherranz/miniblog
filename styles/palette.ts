e1668968da2fc1b61e6136991213282265ddc0f2
Matias Herranz <matias@ipsy.com>
2020-06-24 15:50:40 -0300

    Added styles and color palette files.

---
+++ styles/palette.ts
@@ -0,0 +1,20 @@
+import { red } from '@material-ui/core/colors';
+
+// Create a default pallete.
+const pallete = {
+  primary: {
+    main: '#556cd6',
+  },
+  secondary: {
+    main: '#19857b',
+  },
+  error: {
+    main: red.A400,
+  },
+  background: {
+    // default: '#504c4c',
+    default: '#6a8881d9'
+  }
+};
+
+export default pallete;
