6b0d693cfd488f5f48c77f46b9f17d82f74944a3
Matias Herranz <matias@ipsy.com>
2020-06-25 18:56:26 -0300

    Updated types and component definition.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { FC, ReactElement } from 'react';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
 
-export default function Footer() {
+const Footer: FC = (): ReactElement => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Made by '}
       <MuiLink color="inherit" href="https://linkedin.com/in/matiasherranz">
         Matías Herranz
-      </MuiLink>{' using NextJS, Typescript and MaterialUI'}
+      </MuiLink>
+      {' using NextJS, Typescript and MaterialUI'}
       {'. | '}
       {new Date().getFullYear()}
     </Typography>
   );
-}
+};
+
+export default Footer;
