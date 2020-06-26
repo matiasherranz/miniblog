fc41ec5fc8ada1dbbf75a31c8e749c17349d918b
Matias Herranz <matias@ipsy.com>
2020-06-26 20:02:29 -0300

    Updates to left drawer to show posts brought from reddit.

--- components/LeftDrawer.tsx
+++ components/LeftDrawer.tsx
@@ -1,14 +1,28 @@
 import { FC, ReactElement } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import clsx from 'clsx';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
+import InfiniteScroll from 'react-infinite-scroller';
+
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import AutorenewIcon from '@material-ui/icons/Autorenew';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import { ToggleDrawerProps } from '../util/types';
-import { mainListItems, secondaryListItems } from './ListItems';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import ClearAllIcon from '@material-ui/icons/ClearAll';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { ToggleDrawerProps } from '../util/types';
+import ListItems from './ListItems';
 import { DRAWER_WIDTH } from '../util/constants';
+import {
+  toggleAllPosts,
+  selectPosts,
+  selectIsLoading,
+} from '../lib/slices/redditSlice';
+import { fetchRedditPosts } from '../lib/reddit';
 
 const useStyles = makeStyles((theme) => ({
   drawerPaper: {
@@ -45,6 +59,14 @@ const LeftDrawer: FC<ToggleDrawerProps> = ({
   open,
 }: ToggleDrawerProps): ReactElement => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const { hasMore } = useSelector(selectPosts);
+  const isLoading = useSelector(selectIsLoading);
+
+  const handleDismissAll = () => {
+    setDrawerOpen(false);
+    dispatch(toggleAllPosts());
+  };
 
   return (
     <Drawer
@@ -60,9 +82,32 @@ const LeftDrawer: FC<ToggleDrawerProps> = ({
         </IconButton>
       </div>
       <Divider />
-      <List>{mainListItems}</List>
-      <Divider />
-      <List>{secondaryListItems}</List>
+
+      <InfiniteScroll
+        loadMore={() => dispatch(fetchRedditPosts())}
+        hasMore={hasMore}
+        initialLoad={false}
+        loader={
+          <Box p={1} textAlign="center" hidden={!isLoading} key={0}>
+            <CircularProgress />
+          </Box>
+        }
+        useWindow={false}
+      >
+        <ListItems />
+      </InfiniteScroll>
+      <Box position="fixed" bottom="0" width={'100%'} zIndex="modal">
+        <Button
+          variant="contained"
+          color={hasMore ? 'secondary' : 'primary'}
+          size="large"
+          startIcon={hasMore ? <ClearAllIcon /> : <AutorenewIcon />}
+          onClick={() => handleDismissAll()}
+          fullWidth
+        >
+          {hasMore ? 'Dismiss All' : 'Un-Dismiss!'}
+        </Button>
+      </Box>
     </Drawer>
   );
 };
