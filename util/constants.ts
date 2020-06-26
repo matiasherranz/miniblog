2a33f043a8e840c683ba492ea6865b542f8b6af0
Matias Herranz <matias@ipsy.com>
2020-06-26 20:05:44 -0300

    Updated constants.

--- util/constants.ts
+++ util/constants.ts
@@ -1,5 +1,5 @@
 export const FALLBACK_IMAGE = 'https://via.placeholder.com/350';
 export const BASE_REDDIT_URL = 'https://www.reddit.com';
-export const DRAWER_WIDTH = 240;
+export const DRAWER_WIDTH = 280;
 // Max number of posts per page
-export const PAGE_SIZE = 20;
+export const PAGE_SIZE = 8;
