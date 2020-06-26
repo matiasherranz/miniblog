f383b1f9b695defb9281d3c25ae292d61f5e751c
Matias Herranz <matias@ipsy.com>
2020-06-25 23:10:33 -0300

    Added util/utils.ts with functions to get urls from posts and to validate urls.

---
+++ util/utils.ts
@@ -0,0 +1,25 @@
+import { FALLBACK_IMAGE } from './constants';
+import { Post } from './types';
+
+export const validateUrl = (
+  url: string,
+  defaultUrl?: string
+): string | undefined => {
+  try {
+    new URL(url);
+    return url;
+  } catch (e) {
+    // Invalid URL, return default
+    return defaultUrl;
+  }
+};
+
+export const getMediaUrl = (post: Post): string | undefined => {
+  if (post.is_video) {
+    return post.secure_media.reddit_video.fallback_url;
+  } else if (post.post_hint === 'image') {
+    return post.url;
+  } else {
+    return validateUrl(post.thumbnail, FALLBACK_IMAGE);
+  }
+};
