4043155426233a14ba4ca0d4d963fc73d6d3d8b3
Matias Herranz <matias@ipsy.com>
2020-06-25 23:09:56 -0300

    Added Post type.

--- util/types.ts
+++ util/types.ts
@@ -2,3 +2,23 @@ export type ToggleDrawerProps = {
   setDrawerOpen: (arg0: boolean) => boolean;
   open: boolean;
 };
+
+export type Post = {
+  id: string;
+  title: string;
+  author: string;
+  created: number;
+  name: string;
+  thumbnail: string;
+  url: string;
+  is_video?: boolean;
+  permalink: string;
+  post_hint: string;
+  num_comments: number;
+  is_reddit_media_domain?: boolean;
+  secure_media: {
+    reddit_video: {
+      fallback_url: string;
+    };
+  };
+};
