c529b8f20ce99f6e51a5f8de4e3d88527aad6b48
Matias Herranz <matias@ipsy.com>
2020-06-26 16:03:40 -0300

    Added PAGE_SIZE constant.

--- util/constants.ts
+++ util/constants.ts
@@ -1,3 +1,5 @@
 export const FALLBACK_IMAGE = 'https://via.placeholder.com/350';
 export const BASE_REDDIT_URL = 'https://www.reddit.com';
 export const DRAWER_WIDTH = 240;
+// Max number of posts per page
+export const PAGE_SIZE = 20;
