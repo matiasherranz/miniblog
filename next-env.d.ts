115e12827cc4c78dd6c1175eeb713a5bc44d58b0
Matias Herranz <matias@ipsy.com>
2020-06-26 16:02:03 -0300

    Added global nullable type.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+// Custom global types
+
+// Handy nullable type, inspired from the PossiblyNumber type on this post by @AuMayeung
+// https://levelup.gitconnected.com/typescript-advanced-types-nullable-types-and-type-aliases-847f03b08fae
+type Nullable<T> = T | undefined | null;
